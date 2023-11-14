6780774f9802b342b8e1b9ca00b07d3488c21e50
--replace-all <contactwillswebsites@gmail.com>
2023-11-13 20:36:53 -0700

    Adding window sizing, resizing, and full screen

--- src/script.js
+++ src/script.js
@@ -21,7 +21,7 @@ const scene = new THREE.Scene()
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 'blue' })
+const material = new THREE.MeshBasicMaterial({ color: 'green' })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -67,9 +67,37 @@ scene.add(mesh)
 
 // Viewport
 const sizes = {
-  width: 1000,
-  height: 800
+  width: window.innerWidth,
+  height: window.innerHeight
 }
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+window.addEventListener('dblclick', () => {
+  const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen()
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen()
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen()
+    }
+  }
+})
 
 const aspectRatio = sizes.width / sizes.height
 
@@ -87,6 +115,7 @@ controls.enableDamping = true
 // Renderer
 const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 const clock = new THREE.Clock()
 
